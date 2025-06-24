859f4b72bcc8c56686f9710571b163f29d98cf15
alexsandroapdodev <alexsandrolt1996@gmail.com>
2025-06-23 22:10:56 -0500

    transcricao-de-video-com-ia

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     transcriptionList.innerHTML = "<li>Transcrevendo... aguarde...</li>";
 
     try {
-      const response = await fetch("http://localhost:5678/webhook/youtube/transcribe", {
+      const response = await fetch("https://n8n-deploy-39k2.onrender.com/webhook/youtube/transcribe", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
